1433c5fbeff98853996b2f5fa864d9144ff2bcbb
Moises <moifdez@gmail.com>
2018-11-08 19:33:30 +0100

    Recursos entrada RGB

--- ThingerIO_Rec_Entrada/Rec_Entrada_02/src/main.cpp
+++ ThingerIO_Rec_Entrada/Rec_Entrada_02/src/main.cpp
@@ -14,13 +14,44 @@ ThingerESP8266 thing(usuario, device_Id, device_credentials);
 const char WiFi_ssid[]="************";          //Nombre de red
 const char WiFi_password[]="************";  	//Clave de red
 
-// Definición de variables globales que voy a modificar desde Thingerio
+// Definir las variales de color 
 int Var_rojo;
 int Var_verde;
 int Var_azul;
+String Var_color_hex;
+
 #define PIN_ROJO 12
 #define PIN_VERDE 13
 #define PIN_AZUL 14
+
+// Función para establecer un color
+void setColor(String hexcolor) {
+    // Get rid of '#' and convert it to integer
+    int number = (int) strtol( &hexcolor[1], NULL, 16);
+    // Split them up into r, g, b values
+    int r = (number >> 16) *4;
+    int g = (number >> 8 & 0xFF) *4;
+    int b = (number & 0xFF) * 4;
+    analogWrite(PIN_ROJO, r);
+    analogWrite(PIN_VERDE, g);
+    analogWrite(PIN_AZUL, b);
+}
+
+// Función para testetear el funcionamiento
+void inicio() {
+    setColor("#FF0000");  // red
+    delay(1000);    
+    setColor("#00FF00");  // green
+    delay(1000);
+    setColor("#0000FF");  // blue
+    delay(1000);
+    setColor("#FFFF00");  // yellow
+    delay(1000);  
+    setColor("#500050");  // purple
+    delay(1000);
+    setColor("#00FFFF");  // aqua
+    delay(1000);    
+}
 
 void setup() {
     // put your setup code here, to run once:
@@ -58,28 +89,36 @@ void setup() {
     // Podremos controlar el color con unos deslizadores pero lo más importante aquí es que podemos 
     // modificar el valor de una variable desde Internet
 
-    
-    thing["Var_ColRojo"] << inputValue(Var_rojo, {
-    // se ejecuta cuando el valor cambia - execute some code when the value change
+    thing["Color_Rojo"] << inputValue(Var_rojo, {
+    // se ejecuta cuando el valor cambia execute some code when the value change
     Serial.print("El color Rojo a cambiado desde thingerio a: ");
     Serial.println(Var_rojo);
     analogWrite(PIN_ROJO,Var_rojo);
     });
 
-    thing["Var_ColVerde"] << inputValue(Var_verde, {
-    // se ejecuta cuando el valor cambia - execute some code when the value change
+    thing["Color_Verde"] << inputValue(Var_verde, {
+    // se ejecuta cuando el valor cambia execute some code when the value change
     Serial.print("El color Verde a cambiado desde thingerio a: ");
     Serial.println(Var_verde);
     analogWrite(PIN_VERDE,Var_verde);
     });
     
-    thing["Var_ColAzul"] << inputValue(Var_azul, {
-    // se ejecuta cuando el valor cambia - execute some code when the value change
+    thing["Color_Azul"] << inputValue(Var_azul, {
+    // se ejecuta cuando el valor cambia execute some code when the value change
     Serial.print("El color Azul a cambiado desde thingerio a: ");
     Serial.println(Var_azul);
     analogWrite(PIN_AZUL,Var_azul);
     });
 
+    thing["Color_Hexadecimal"] << inputValue(Var_color_hex, {
+    // se ejecuta cuando el valor cambia execute some code when the value change
+    Serial.print("El color a cambiado desde thingerio a: ");
+    Serial.println(Var_color_hex);
+    setColor(Var_color_hex);  // color en Hexadeimal 
+    });
+
+
+    inicio();
 }    
 
 void loop() {
